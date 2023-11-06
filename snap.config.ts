bb7f746abe9f36d6ff53ee96fb2c6bbd7f2ef6f5
muji <muji@tmpfs.org>
2023-11-06 08:51:16 +0800

    Update all dependencies, config file for snap.

---
+++ snap.config.ts
@@ -0,0 +1,14 @@
+import type { SnapConfig } from '@metamask/snaps-cli';
+
+const config: SnapConfig = {
+  bundler: 'webpack', // default: 'browserify'
+  input: 'snap.js',
+  output: {
+    path: 'dist',
+  },
+  server: {
+    port: 9000,
+  },
+};
+
+export default config;
